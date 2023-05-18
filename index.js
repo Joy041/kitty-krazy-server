18343c455b89f1ea6a7636b61554facfb26f6481
Joy Purkistha <joypurokisthosh@gmail.com>
2023-05-19 03:41:31 +0600

    products find by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -15,41 +15,48 @@ const uri = `mongodb+srv://${process.env.MD_USERNAME}:${process.env.MD_PASSWORD}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
 
-    const toyDatabase = client.db('toyDB').collection('toys') 
+        const toyDatabase = client.db('toyDB').collection('toys')
 
 
-    app.get('/products', async(req, res) => {
-        const product = toyDatabase.find()
-        const result = await product.toArray()
-         res.send(result)
-      })
+        app.get('/products', async (req, res) => {
+            const product = toyDatabase.find()
+            const result = await product.toArray()
+            res.send(result)
+        })
 
-    app.post('/toys', async(req, res) => {
-        const toy = req.body;
-        console.log('new user', toy)
-        const result = await toyDatabase.insertOne(toy)
-        res.send(result)  
-    })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyDatabase.findOne(query)
+            res.send(result)
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.post('/toys', async (req, res) => {
+            const toy = req.body;
+            console.log('new user', toy)
+            const result = await toyDatabase.insertOne(toy)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
